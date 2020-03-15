88f4e88d09ba1963ffaf66374ffe9779aec67e9c
wangmingda <wang521763@163.com>
2020-03-15 11:08:04 +0800

    注册使用Vant组件

--- src/main.js
+++ src/main.js
@@ -2,7 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import Vant from 'vant' // 引入vant组件库
+import 'vant/lib/index.css' // 引入vant组件的样式
+Vue.use(Vant) // 注册vant组件
+// 以上做法是完整导入Vant的模式，以后优化代码时可以按需导入
 Vue.config.productionTip = false
 
 new Vue({
