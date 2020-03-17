cd6b443fc4ad722cde7e3a2f21aa69ae23137e83
wangmingda <wang521763@163.com>
2020-03-17 23:34:05 +0800

    登录页面完成

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,6 +1,19 @@
 <template>
   <div class="container">
     <van-nav-bar title="登陆" left-arrow="" @click-left="$router.back()"></van-nav-bar>
+    <!-- 登录布局 -->
+    <van-cell-group>
+      <!-- 登录手机号 -->
+    <van-field label="手机号" placeholder="请输入手机号"/>
+    <!-- 验证码 -->
+    <van-field label="验证码" placeholder="请输入验证码">
+    <van-button slot="button" size="small" type="primary">发送验证码</van-button>
+    </van-field>
+    </van-cell-group>
+    <!-- 登录按钮 -->
+    <div class="login-box">
+      <van-button type="info" block round size="small">登陆</van-button>
+    </div>
   </div>
 </template>
 
@@ -11,5 +24,7 @@ export default {
 </script>
 
 <style>
-
+.login-box {
+  padding: 5px;
+}
 </style>
