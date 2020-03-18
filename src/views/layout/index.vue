b03e85273b7e418df8214d3078c2c5caba207e48
wangmingda <wang521763@163.com>
2020-03-18 15:05:53 +0800

    首页基本tabs结构完成

--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <!-- 1-导航组件 采用Vant组件导航 fixed表示固定-->
     <!-- 当页面地址是/user的时候应该隐藏顶部栏 用v-if-->
-    <van-nav-bar title="黑马头套" right-text="搜索" fixed v-if="$route.path !== '/user'" @click-right="$router.push('/search')"></van-nav-bar>
+    <van-nav-bar title="黑马偷笑" right-text="搜索" fixed v-if="$route.path !== '/user'" @click-right="$router.push('/search')"></van-nav-bar>
     <!-- 2-放置二级路由容器 -->
     <!-- 判断当前地址如果是/user，就给一个noTop Class -->
     <div class="my-wrapper" :class="{noTop: $route.path==='/user'}">
